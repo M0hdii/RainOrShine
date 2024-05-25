ab0dd00011f94aff25dc15ee214b9db839d98ae7
M0hdii <mehdiomar71@gmail.com>
2024-05-25 11:47:22 +0100

    fix

--- server.mjs
+++ server.mjs
@@ -12,6 +12,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 // Serve static files from the 'Climacast' directory
 app.use(express.static(join(__dirname, 'frontend')));
 
+
 // Define a default route to serve index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
